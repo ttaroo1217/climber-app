fc0194f7a4abbcfa8990fdf6268e6cf9e5fb375a
ttaroo1217 <69450720+ttaroo1217@users.noreply.github.com>
2020-11-17 11:33:58 +0900

    usersテーブル作成

---
+++ app/models/user.rb
@@ -0,0 +1,41 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :name
+  end
+
+  with_options numericality: { other_than: 0 } do
+    validates :sex_id
+    validates :area_id
+    validates :weight_id
+    validates :belay_exp_id
+  end
+
+  # has_many :room_users
+  # has_many :rooms, through: room_users
+  # has_many :messages
+  
+
+  # ====================自分がフォローしているユーザーとの関連 ===================================
+  #フォローする側のUserから見て、フォローされる側のUserを(中間テーブルを介して)集める。なので親はfollowing_id(フォローする側)
+  # has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  # 中間テーブルを介して「follower」モデルのUser(フォローされた側)を集めることを「following」と定義
+  # has_many :followings, through: :active_relationships, source: :follower
+  # ========================================================================================
+
+  # ====================自分がフォローされるユーザーとの関連 ===================================
+  #フォローされる側のUserから見て、フォローしてくる側のUserを(中間テーブルを介して)集める。なので親はfollower_id(フォローされる側)
+  # has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  # 中間テーブルを介して「following」モデルのUser(フォローする側)を集めることを「follower」と定義
+  # has_many :followers, through: :passive_relationships, source: :following
+  # =======================================================================================
+
+  # def followed_by?(user)
+  #   今自分(引数のuser)がフォローしようとしているユーザー(レシーバー)がフォローされているユーザー(つまりpassive)の中から、引数に渡されたユーザー(自分)がいるかどうかを調べる
+  #   passive_relationships.find_by(following_id: user.id).present?
+  # end
+  
+end
