8ef9a5b7d409b330f31f0f71097f4cc0b4a6f1b1
ttaroo1217 <69450720+ttaroo1217@users.noreply.github.com>
2020-11-20 14:40:02 +0900

    Active storageインストール
    ・gemインストール
    ・Active storageインストール
    ・テーブル作成
    ・userモデルにアソシエーション追加
    ・新規登録ページ編集
    ・ストロングパラメーターにkeyを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :self_introduction, :sex_id, :area_id, :weight_id, :belay_exp_id, :climb_type])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :self_introduction, :sex_id, :area_id, :weight_id, :belay_exp_id, :climb_type, :image])
   end
   
 end
