a66f8048320c6c6b929f71693f4d63b4defcf88e
ttaroo1217 <69450720+ttaroo1217@users.noreply.github.com>
2020-11-23 12:37:09 +0900

    ユーザ編集ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,20 @@ class UsersController < ApplicationController
   end
 
   def show
-    # @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
+
+  # def update
+  #   if current_user.update(user_params)
+  #     redirect_to root_path
+  #   else
+  #     render :edit
+  #   end
+  # end
+
+  # private
+
+  # def user_params
+  #   params.require(:user).permit(:name, :self_introduction, :sex_id, :area_id, :weight_id, :belay_exp_id, :climb_type, :image)
+  # end
 end
