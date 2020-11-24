ee8de30571060be5584d0ad22d06b2a9ead7993b
ttaroo1217 <69450720+ttaroo1217@users.noreply.github.com>
2020-11-24 12:54:56 +0900

    フォロー機能実装/プロフィール編集ボタン修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,19 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    # @users = User.all
   end
 
+  # ==============追加================
+  def follows
+    user = User.find(params[:id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:id])
+    @users = user.followers
+  end
+# ==============追加================
 
 end
