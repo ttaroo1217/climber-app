60a222799c90169276d91cf13b5667206125f0d6
ttaroo1217 <69450720+ttaroo1217@users.noreply.github.com>
2021-02-02 11:58:56 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,26 @@ class ApplicationController < ActionController::Base
   
 
   private
+  # binding.pry
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :self_introduction, :sex_id, :area_id, :weight_id, :climbing_type, :image])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:name, :self_introduction, :sex_id, :area_id, :weight_id, :climbing_type, :image])
+  # end
+
+  
+# climbing_typeあり
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :self_introduction, :sex_id, :area_id, :weight_id, :climbing_type, :image])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:name, :self_introduction, :sex_id, :area_id, :weight_id, :climbing_type, :image])
+  # end
+
   def configure_permitted_parameters
-    # binding.pry
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :self_introduction, :sex_id, :area_id, :weight_id, :climbing_type, :image])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :self_introduction, :sex_id, :area_id, :weight_id, :climbing_type, :image])
+    binding.pry
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :self_introduction, :sex_id, :area_id, :weight_id, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :self_introduction, :sex_id, :area_id, :weight_id, :image])
+  end
+
+  def climbing_type_params
+    params.require(:user).permit(climbing_types: [])
   end
 end
